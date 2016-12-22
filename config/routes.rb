e0f864e2230237cf1dc3e9c72e4f48dcd99f728b
Michele de Bruyn <debruyn.mm@gmail.com>
2016-12-22 15:10:30 +0100

    Add routes to foodshare

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :dashboard, only: [:show]
+
+  resources :meals, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:index, :new, :create]
+    resources :orders, only: [:index, :new, :create] do
+      member do
+        patch :accept
+        patch :decline
+      end
+    end
+  end
 end
