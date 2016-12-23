f0edaebec0d471544af3a7ba149d56066fc4b96e
Michele de Bruyn <debruyn.mm@gmail.com>
2016-12-23 16:53:03 +0100

    Add seeds file

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,6 +1,6 @@
 class Meal < ApplicationRecord
-  belongs_to :creator, class_name: "User"
-  has_many :reviews
-  has_many :pictures
-  has_many :customers
+  belongs_to :creator, class_name: "User", foreign_key: "creator_id"
+  has_many :reviews, dependent: :destroy
+  has_many :pictures, dependent: :destroy
+  has_many :customers, dependent: :destroy
 end
