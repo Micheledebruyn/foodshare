dc51a74224cd72fb5acf48643170f5d3a13eaf15
Michele de Bruyn <debruyn.mm@gmail.com>
2016-12-23 17:28:00 +0100

    Create new meal

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -8,8 +8,20 @@ class MealsController < ApplicationController
   end
 
   def new
+    @meal = Meal.new
   end
 
   def create
+    @meal = Meal.new(meal_params)
+    @meal.creator_id = current_user.id
+    @meal.save!
+    redirect_to meals_path
+  end
+
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:title, :description, :quantity, :start_date, :end_date)
   end
 end
