2ad4451b427ea778f9d701c0451797eb551dbadd
Michele de Bruyn <debruyn.mm@gmail.com>
2017-01-04 14:26:02 +0100

    Add edit function and view

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,10 +1,11 @@
 class MealsController < ApplicationController
+  before_action :find_meal, only: [:show, :edit, :update]
+
   def index
     @meals = Meal.all
   end
 
   def show
-    @meal = Meal.find(params[:id])
   end
 
   def new
@@ -18,8 +19,22 @@ class MealsController < ApplicationController
     redirect_to meals_path
   end
 
+  def edit
+  end
+
+  def update
+    if @meal.update(meal_params)
+      redirect_to meal_path(@meal)
+    else
+      render :edit
+    end
+  end
 
   private
+
+  def find_meal
+    @meal = Meal.find(params[:id])
+  end
 
   def meal_params
     params.require(:meal).permit(:title, :description, :quantity, :start_date, :end_date)
