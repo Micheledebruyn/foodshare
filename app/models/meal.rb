fe17351eff69e4e79d961091ba3ee6f9e88da976
Michele de Bruyn <debruyn.mm@gmail.com>
2017-01-06 15:59:10 +0100

    Remove pictures table

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,6 +2,5 @@ class Meal < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   has_many :reviews, dependent: :destroy
   has_many :customers, dependent: :destroy
-  has_many :pictures, dependent: :destroy
 
 end
--- db/migrate/20161220144412_add_reference_to_picturs.rb
+++
@@ -1,6 +0,0 @@
-class AddReferenceToPicturs < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :pictures, :meal_id
-    add_reference :pictures, :meal, index: true, foreign_key: true
-  end
-end
